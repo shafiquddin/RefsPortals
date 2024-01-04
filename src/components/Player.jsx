52871eec49345d62ddbfa00112044bf48b0c2cf1
shafiquddin <shafiquddin2k@gmail.com>
2024-01-04 18:33:32 +0530

    refs included

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,25 +1,19 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export default function Player() {
-  const [palyerName,setPlayerName] = useState(null);
-  const [submitted,setSubmitted] = useState(false)
+  const [submitted,setSubmitted] = useState('')
+  const playerName = useRef();
 
   const clickHandler = () =>{
-    if(palyerName.trim().length > 0){
-      setSubmitted(true);
-    }
-  }
-
-  const changeHandler = (event) =>{
-    setSubmitted(false);
-    setPlayerName(event.target.value);
+    setSubmitted(playerName.current.value);
+    playerName.current.value = ''
   }
 
   return (
     <section id="player">
-      <h2>Welcome {submitted ? palyerName : 'unknown entity'}</h2>
+      <h2>Welcome { submitted ? submitted : 'unknown entity'}</h2>
       <p>
-        <input type="text" onChange={changeHandler} value={palyerName}/>
+        <input type="text" ref={playerName}/>
         <button onClick={clickHandler}>Set Name</button>
       </p>
     </section>
