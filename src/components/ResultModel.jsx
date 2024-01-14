77fabeff282bdb2249a251420ae845fa586249d2
shafiquddin <shafiquddin2k@gmail.com>
2024-01-14 17:46:50 +0530

    ImperativeHandle Added

--- src/components/ResultModel.jsx
+++ src/components/ResultModel.jsx
@@ -1,7 +1,15 @@
-import { forwardRef } from "react";
+import { forwardRef, useImperativeHandle, useRef } from "react";
 
 const ResultModel = forwardRef(function ResultModel({result,targetTime},ref){
-    return <dialog ref={ref} className="result-modal">
+    const dialog = useRef(); 
+    useImperativeHandle(ref,()=>{
+        return {
+            open(){
+                dialog.current.showModal();
+            }
+        }
+    })
+    return <dialog ref={dialog} className="result-modal">
         <h2>You {result}</h2>
         <p>Your target time was <strong>{targetTime} seconds.</strong></p>
         <p>You stopped the timer with <strong>X seconds left</strong></p>
